d06ff1f08677e2c806dfdf2828ff6122330d47ee
Shivam Sharma <meshivam81@gmail.com>
2025-01-28 12:39:51 +0530

    fix: missing export for data backup

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './core/connection.js';
 export * from './utils/schema-validation.js';
 export * from './core/queryCaching.js';
 export * from './core/auditLogging.js';
+export * from './core/dataBackup.js';
