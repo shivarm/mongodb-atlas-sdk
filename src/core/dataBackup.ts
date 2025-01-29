c2084ba54e7c3b01859c38d9cd1882fe23d6fd1b
Shivam Sharma <meshivam81@gmail.com>
2025-01-29 14:13:20 +0530

    feat: ensure dircetory exist

--- src/core/dataBackup.ts
+++ src/core/dataBackup.ts
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
 import fs from 'fs/promises';
+import path from 'path';
 import { logger } from '../logger.ts';
 
 export class Backup {
   async backupModel(model: mongoose.Model<any>, filePath: string): Promise<void> {
     try {
+      const directory = path.dirname(filePath);
+      await fs.mkdir(directory, { recursive: true });
       const collectionName = model.collection.name;
       // Retrieve all documents as plain objects
       const data = await model.find({}).lean().exec();
