0f5e3a1f95569a556960c694defe581bb9282ef6
Shivam Sharma <meshivam81@gmail.com>
2025-01-30 13:42:21 +0530

    feat(db): backup with option

--- src/core/dataBackup.ts
+++ src/core/dataBackup.ts
@@ -3,14 +3,21 @@ import fs from 'fs/promises';
 import path from 'node:path';
 import { logger } from '../logger.ts';
 
+interface BackupOptions {
+  fields?: string[];
+}
+
 export class Backup {
-  async backupModel(model: mongoose.Model<any>, filePath: string): Promise<void> {
+  async backupModel(model: mongoose.Model<any>, filePath: string, options: BackupOptions = {}): Promise<void> {
     try {
+      const { fields } = options;
       const directory = path.dirname(filePath);
       await fs.mkdir(directory, { recursive: true });
       const collectionName = model.collection.name;
-      // Retrieve all documents as plain objects
-      const data = await model.find({}).lean().exec();
+
+      // Retrieve all documents as plain objects and specified fields
+      const projection = fields ? fields.reduce((acc, field) => ({ ...acc, [field]: 1 }), {}) : {};
+      const data = await model.find({}, projection).lean().exec();
       await fs.writeFile(filePath, JSON.stringify(data, null, 2));
       logger.info(`Backup of collection ${collectionName} completed successfully.`);
     } catch (error) {
