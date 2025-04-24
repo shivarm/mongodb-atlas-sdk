503b6010c38d530aa115805f1e7b8e29498f12fe
Shivam Sharma <meshivam81@gmail.com>
2025-04-24 17:16:36 +0530

    fix: extention for entry point (#35)

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ export * from './utils/schema-validation.js';
 export * from './core/queryCaching.js';
 export * from './core/auditLogging.js';
 export * from './core/dataBackup.js';
-export * from './core/pagination.ts';
-export * from './core/dataAgrregation.ts';
+export * from './core/pagination.js';
+export * from './core/dataAgrregation.js';
